c605e84d4f8b2df7faf06ae67fc488740d01d925
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-24 04:05:01 +0800

    add edit

--- app/Models/Jar.php
+++ app/Models/Jar.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Jar extends Model
 {
     use HasFactory;
+    protected $fillable = ['jar_name', 'cost', 'savings'];
+    protected $attributes = [
+        'savings' => 0.00,
+    ];
+    // Override the default setter for the "cost" field
+    public function setCostAttribute($value)
+    {
+        $this->attributes['cost'] = $value;
+        $this->updateRemaining();
+    }
+
+    // Override the default setter for the "savings" field
+    public function setSavingsAttribute($value)
+    {
+        $this->attributes['savings'] = $value;
+        $this->updateRemaining();
+    }
+
+    // Define an accessor for the "remaining" field
+    public function getRemainingAttribute()
+    {
+        return $this->calculateRemaining();
+    }
+
+    // Update the "remaining" field based on the "cost" and "savings" fields
+    private function updateRemaining()
+    {
+        $this->attributes['remaining'] = $this->calculateRemaining();
+    }
+
+    // Calculate the value of the "remaining" field
+    private function calculateRemaining()
+    {
+        return $this->cost - $this->savings;
+    }
 }
