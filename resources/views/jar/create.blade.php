250c5bb6aa3d40294418c8ade747fb0c0b9abca3
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-27 01:28:15 +0800

    redesign

--- resources/views/jar/create.blade.php
+++ resources/views/jar/create.blade.php
@@ -1,3 +1,6 @@
+@extends('jar.layout')
+
+@section('content')
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
@@ -23,12 +26,21 @@
 <form action="{{ route('jar.store') }}" method="POST">
     @csrf
 
-    Name:
-    <input type="text" name="jar_name"><br/>
+    <div class="form-group">
+        <label for="jar_name">Name:</label>
+        <input type="text" name="jar_name" id="jar_name" class="form-control">
+    </div>
     
-    Price: 
-    <input type="number" name="cost">
+    <div class="form-group">
+        <label for="description">Description:</label>
+        <input type="text" name="description" id="description" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="cost">Price:</label>
+        <input type="number" name="cost" id="cost" class="form-control">
+    </div>
   
-     <button type="submit">Submit</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-   
+@endsection
