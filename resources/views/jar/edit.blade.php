f52c72441f4b75f899c2a60aaea4efb94d4a0c16
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-27 00:54:01 +0800

    updated

---
+++ resources/views/jar/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('jar.layout')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Edit Jar - {{ $jar->jar_name }}</h2>
+            <form action="{{ route('jar.update', $jar->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="savings">Savings</label>
+                    <input type="number" id="savings" name="savings" class="form-control" value="{{ $jar->savings }}">
+                </div>
+                <div class="form-group">
+                    <label for="jar_name">Product to Buy</label>
+                    <input type="text" id="jar_name" name="jar_name" class="form-control" value="{{ $jar->jar_name }}" readonly>
+                </div>
+                <div class="pull-right">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                    <a class="btn btn-secondary" href="{{ route('jar.index') }}">Back</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+@endsection
--- resources/views/jar/index.blade.php
+++ resources/views/jar/index.blade.php
@@ -1,24 +1,40 @@
-@extends ('jar.layout')
+@extends('jar.layout')
 
 @section('content')
 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-10">
-            <div class="pull-left">
+            <div class="text-left">
                 <h2>Wishlist Crud</h2>
             </div>
-            <div class="pull-right">
+            <div class="text-right">
                 <a class="btn btn-success" href="{{ route('jar.create') }}">Create New Jar</a>
             </div>
         </div>
     </div>
 
-    @foreach($jar as $jars)
-    {{ $jars->jar_name }}
-    {{$jars->cost}}
-    <a class="btn btn-primary" href="{{ route('jar.edit',$jars->id) }}">Edit</a>
-    @endforeach
+    <div class="row mt-4">
+        <div class="col-lg-10">
+            @foreach($jar as $jars)
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $jars->jar_name }}</h5>
+                    <p class="card-text">{{$jars->description}}</p>
+                    <p class="card-text">Cost: {{$jars->cost}}</p>
+                    <p class="card-text">Remaining:{{$jars->remaining}}</p>
+                    <a class="btn btn-primary" href="{{ route('jar.edit', $jars->id) }}">Edit</a>
+                    
+                    <form action="{{ route('jar.destroy', $jars->id) }}" method="POST" class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 
-
+@endsection
