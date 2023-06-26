fa5ff8187b66957abb8b046791548c1e1bc91786
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-27 05:16:56 +0800

    re design

--- resources/views/jar/edit.blade.php
+++ resources/views/jar/edit.blade.php
@@ -3,26 +3,33 @@
 @section('content')
 
 <div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Edit Jar - {{ $jar->jar_name }}</h2>
-            <form action="{{ route('jar.update', $jar->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                Balance:{{$jar->savings}}
-                <div class="form-group">
-                    <label for="savings">Savings</label>
-                    <input type="number" id="savings" name="savings" class="form-control" value="0">
-                </div>
-                <div class="form-group">
-                    <label for="jar_name">Product to Buy</label>
-                    <input type="text" id="jar_name" name="jar_name" class="form-control" value="{{ $jar->jar_name }}" readonly>
-                </div>
-                <div class="pull-right">
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    <a class="btn btn-secondary" href="{{ route('jar.index') }}">Back</a>
-                </div>
-            </form>
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h2 class="card-title">Edit Jar - {{ $jar->jar_name }}</h2>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('jar.update', $jar->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="savings">Balance:</label>
+                        <span class="font-weight-bold">{{ $jar->savings }}</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="savings">Savings:</label>
+                        <input type="number" id="savings" name="savings" class="form-control" value="0">
+                    </div>
+                    <div class="form-group">
+                        <label for="jar_name">Product to Buy:</label>
+                        <input type="text" id="jar_name" name="jar_name" class="form-control" value="{{ $jar->jar_name }}" readonly>
+                    </div>
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <a class="btn btn-secondary" href="{{ route('jar.index') }}">Back</a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
