284b510dc4d53d4122c96110799f0baed68801f3
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-28 03:43:37 +0800

    add background image

--- resources/views/jar/layout.blade.php
+++ resources/views/jar/layout.blade.php
@@ -3,12 +3,18 @@
 <head>
     <title>Wishlist App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background: url("/images/jar_background.jpg");
+            background-size:cover;
+        }
+    </style>
 </head>
 <body>
-    
-<div class="container">
+
+<div class="container bg-white p-5 mt-5">
     @yield('content')
 </div>
-    
+
 </body>
 </html>
