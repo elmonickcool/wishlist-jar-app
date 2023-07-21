3b7972eb5b92236135feb6c2f3c3e576bd198c96
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-22 00:18:37 +0800

    buy with total savings button added

--- resources/views/jar/index.blade.php
+++ resources/views/jar/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-
 <div class="row justify-content-center">
     <div class="col">
         <h2>📃Wishlist</h2>
@@ -18,50 +17,67 @@
         @foreach($jar as $jars)
         <div class="card mb-3 mx-auto">
             <div class="card-body">
-            @if ($jars->image)
-                    <div>
-                        <img src="{{ asset('images/'.$jars->image) }}" alt="Jar Image" class="img-fluid">
+                <div class="row">
+                    <div class="col-md-4">
+                        @if ($jars->image)
+                            <div>
+                                <img src="{{ asset('images/'.$jars->image) }}" alt="Jar Image" class="img-thumbnail rounded mx-auto d-block">
+                            </div>
+                        @endif
                     </div>
-                    @endif
-                <a href="{{$jars->link}}">
-                    <h5 class="card-title">{{ $jars->jar_name }}</h5>
-                </a>
-                <p class="card-text">Category: {{ $jars->category }}</p>
-                <p class="card-text">{{ $jars->description }}</p>
-                <p class="card-text">Cost: <strong>{{ number_format($jars->cost, 2) }}</strong></p>
-                <p class="card-text">Savings: <strong>{{ number_format($jars->savings, 2) }}</strong></p>
+                    <div class="col-md-8">
+                        <a href="{{$jars->link}}">
+                            <h5 class="card-title">{{ $jars->jar_name }}</h5>
+                        </a>
+                        <p class="card-text">Category: {{ $jars->category }}</p>
+                        <p class="card-text">{{ $jars->description }}</p>
+                        <p class="card-text">Cost: <strong>{{ number_format($jars->cost, 2) }}</strong></p>
+                        <p class="card-text">Savings: <strong>{{ number_format($jars->savings, 2) }}</strong></p>
 
+                        @if ($jars->buy)
+                            <div class="alert alert-success">
+                                <p class="card-text">You bought {{ $jars->jar_name }}</p>
+                            </div>
+                        @elseif ($jars->remaining == 0)
+                            <div class="alert alert-primary">
+                                <p class="card-text">You can buy {{ $jars->jar_name }}</p>
+                            </div>
+                            @if ($totalSavings >= $jars->cost)
+                                <form action="{{ route('jar.buy', $jars->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success">Buy</button>
+                                </form>
+                            @endif
+                            @if ($totalSavings > $jars->cost && $jars->savings < $jars->cost)
+                                <form action="{{ route('jar.buy', $jars->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-info">Buy with Total Savings</button>
+                                </form>
+                            @endif
+                        @else
+                            <div class="alert alert-warning">
+                                <p class="card-text">You need {{ $jars->remaining }} to buy {{ $jars->jar_name }}</p>
+                            </div>
+                            <a class="btn btn-primary" href="{{ route('jar.edit', $jars->id) }}">Prioritize</a>
+                            @if ($totalSavings > $jars->cost && $jars->savings < $jars->cost)
+                                <form action="{{ route('jar.buy', $jars->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-info">Buy with Total Savings</button>
+                                </form>
+                            @endif
+                        @endif
 
-                @if ($jars->buy)
-                <div class="alert alert-success">
-                    <p class="card-text">You bought {{ $jars->jar_name }}</p>
+                        <form action="{{ route('jar.destroy', $jars->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
                 </div>
-                @elseif ($jars->remaining == 0)
-                <div class="alert alert-warning">
-                    <p class="card-text">You can buy {{ $jars->jar_name }}</p>
-                </div>
-                <form action="{{ route('jar.buy', $jars->id) }}" method="POST" class="d-inline">
-                    @csrf
-                    <button type="submit" class="btn btn-success">Buy</button>
-                </form>
-                @else
-                <div class="alert alert-warning">
-                    <p class="card-text">You need {{ $jars->remaining }} to buy {{ $jars->jar_name }}</p>
-                </div>
-                <a class="btn btn-primary" href="{{ route('jar.edit', $jars->id) }}">Prioritize</a>
-                @endif
-
-
-                <form action="{{ route('jar.destroy', $jars->id) }}" method="POST" class="d-inline">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
             </div>
         </div>
         @endforeach
     </div>
 </div>
-
 
 @endsection
